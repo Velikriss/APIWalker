b4f218220ad930f522ddbbc3cfd71c85626bf039
Nathan Turinski <nathan.turinski@gmail.com>
2017-05-25 17:10:56 -0500

    Removes configuration defaults that I used for testing

--- helpers/config.js
+++ helpers/config.js
@@ -5,8 +5,8 @@ if (process.env.NODE_ENV === 'development') {
 	config.issueTypes = '[bugs|stories|tasks]';
 } else if (process.env.NODE_ENV === 'configuration') {
 	// enter configuration information here
-	config.apiRoot = 'http://www.example.com/api/v1';
-	config.issueTypes = '[bugs|stories|tasks]';
+	config.apiRoot = '';
+	config.issueTypes = '';
 }	else {
 	config = null;
 }
